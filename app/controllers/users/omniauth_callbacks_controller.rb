5eaee418157fa55ffaf538fb8a888c8d41d28c95
Whoopee <139693279+GPFE@users.noreply.github.com>
2024-11-25 16:36:27 +0100

    add google oauth 2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should also create an action method in this controller like this:
   # def twitter
   # end
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    user = User.from_omniauth(request.env['omniauth.auth'])
 
+    if user.present?
+      sign_out_all_scopes
+      flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      # Useful for debugging login failures. Uncomment for development.
+      # session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: '#{auth.info.email} is not authorized.'
+      redirect_to new_user_registration_url
+    end
+  end
   # More info at:
   # https://github.com/heartcombo/devise#omniauth
 
@@ -22,6 +36,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   # protected
+  private
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
