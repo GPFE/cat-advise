653af38ff8e4c8a1ade643b108a94f14612054a4
Whoopee <139693279+GPFE@users.noreply.github.com>
2024-11-25 19:36:31 +0100

    display profile photos correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ApplicationRecord
       user.username = "#{Faker::Artist.name} #{rand(99999)}"
       user.password = Devise.friendly_token[0, 20]
       user.full_name = auth.info.name
-      user.avatar_url = auth.info.name
+      user.avatar_url = auth.info.image
       # user.skip_confirmation!
     end
   end
